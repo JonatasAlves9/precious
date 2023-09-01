fbd7547f21949f545dda9a692cf2a030e57c95f9
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-09-01 18:17:07 -0300

    feat: create datalist of tasks

--- src/presentation/modules/tasks/styles.ts
+++ src/presentation/modules/tasks/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {tokens} from "@fluentui/react-components";
+import {tokens, Text} from "@fluentui/react-components";
 
 export const Wrapper = styled.div`
   display: flex;
@@ -8,7 +8,23 @@ export const Wrapper = styled.div`
 `;
 
 export const Content = styled.div`
-  background-color: ${tokens.colorBrandBackgroundInverted};
+  background-color: ${tokens.colorNeutralForegroundInverted};
   width: 100%;
   height: 100%;
+`;
+
+export const HeaderContent = styled.div`
+  display: flex;
+  padding: 20px;
+`;
+
+export const ViewTexts = styled.div`
+`;
+export const TextHeaderContent = styled(Text)`
+  font-weight: ${tokens.fontWeightBold};
+`;
+
+export const DateHeaderContent = styled(Text)`
+  font-size: ${tokens.fontSizeBase100};
+  margin-left: 10px;
 `;
